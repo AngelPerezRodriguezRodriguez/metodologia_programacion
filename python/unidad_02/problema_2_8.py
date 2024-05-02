c81ab1e8ea1b802deb8a6a5bf22e21b011202586
Angel Pérez Rodríguez Rodríguez <angelperezrdguez@gmail.com>
2024-05-01 23:07:50 -0600

    feat: problema 2.8 - java, python

---
+++ python/unidad_02/problema_2_8.py
@@ -0,0 +1,65 @@
+"""
+El programa, dado como dato el monto de la compra de un cliente, determina lo
+que el mismo debe pagar teniendo en cuenta una serie de descuentos.
+"""
+
+def aplicar_descuento(total_compra):
+
+    if total_compra < 500:
+
+        descuento = 0
+
+        nuevo_total = total_compra
+
+    elif total_compra <= 1000:
+
+        descuento = 0.05
+
+        nuevo_total = total_compra - total_compra * descuento
+
+    elif total_compra <= 7000:
+
+        descuento = 0.11
+
+        nuevo_total = total_compra - total_compra * descuento
+
+    elif total_compra <= 15000:
+
+        descuento = 0.18
+
+        nuevo_total = total_compra - total_compra * descuento
+
+    else:
+
+        descuento = 0.25
+        
+        nuevo_total = total_compra - total_compra * descuento
+    
+    return descuento, nuevo_total
+
+def main():
+
+    try:
+
+        total_compra = float(input("Total de compra: "))
+
+        if total_compra <= 0:
+
+            print("Error: el total de compra debe ser mayor que cero.")
+            return
+
+    except ValueError as e:
+
+        print(e)
+        return
+    
+    descuento, nuevo_total = aplicar_descuento(total_compra)
+
+    
+    print(f"{total_compra} u tiene {descuento * 100} % de descuento,")
+
+    print(f"por lo tanto, el nuevo total es de {nuevo_total:2f} u")
+
+if __name__ == '__main__':
+
+    main()
